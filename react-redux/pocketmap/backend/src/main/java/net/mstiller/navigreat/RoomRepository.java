31fddfc8d63f938efc8ae1e7fae537daca667777
Matthew Stiller <mstiller@ggc.edu>
2020-05-20 14:33:04 -0400

    Swapped customers for Room objects. Enabled preliminary sample data creation on application runtime.

--- react-redux/pocketmap/backend/src/main/java/net/mstiller/navigreat/RoomRepository.java
+++ react-redux/pocketmap/backend/src/main/java/net/mstiller/navigreat/RoomRepository.java
@@ -2,20 +2,18 @@ package net.mstiller.navigreat;
 
 import java.util.List;
 
-import net.mstiller.navigreat.objects.Customer;
+import net.mstiller.navigreat.objects.Room;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 // allows CORS policy
-@CrossOrigin(allowedHeaders={"*"})
+@CrossOrigin(allowedHeaders = {"*"})
 // defines the REST path; e.g. "localhost:8080/customers"
-@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
-public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+@RepositoryRestResource(collectionResourceRel = "rooms", path = "rooms")
+public interface CustomerRepository extends PagingAndSortingRepository<Room, Long> {
 	
-	List<Customer> findByFirstName(@Param("name") String name);
-	
-	List<Customer> findByLastName(@Param("name") String name);
+	List<Room> findRoom(@Param("name") String name);
 	
 }
