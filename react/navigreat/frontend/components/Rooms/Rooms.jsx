ed3e40170facda06ecdcd4568e45c0d80fb07384
Matthew Stiller <mstiller@ggc.edu>
2020-05-26 12:03:25 -0400

    Fixed Rooms fetching updates constantly.

--- react/navigreat/frontend/components/Rooms/Rooms.jsx
+++ react/navigreat/frontend/components/Rooms/Rooms.jsx
@@ -12,11 +12,18 @@ export default function Rooms() {
   // TODO add caching so a refresh doesn't necessarily need to occur.
   useEffect(() => {
     actions.refreshRooms();
-  });
+  }, []);
+  // ? the empty array here ensures the component
+  // ? doesn't continually re-render.
+  
+  // TODO add a refresh button?
+  // useEffect(() => {
+  //   actions.refreshRooms();
+  // }, [state.rooms.length]);
 
   return (
     <View style={{ padding: "10px" }}>
-      <Card containerStyle={{ padding: 0 }}>
+      <Card>
         {state.rooms.map((room, i) => {
           return <ListItem key={i} title={room.name} />;
         })}
