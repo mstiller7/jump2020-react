84f499942955788f00abea26e97553dc6d61fce3
Matthew Stiller <mstiller@ggc.edu>
2020-06-30 15:21:32 -0400

    Remove action TS defs. Add search to state.

--- react/navigreat/frontend/overmind/state.ts
+++ react/navigreat/frontend/overmind/state.ts
@@ -1,23 +1,17 @@
-// defines the State. like a class?
-type State = {
-  user: string;
-  rooms: Array<Room>;
-  file: any;
-};
-
-type Room = {
-  title: String;
-  imageRef: String;
-  imageData: String;
-  building: number;
-  floor: number;
-  capacity: number;
-  reservations: Array<any>;
-};
+// type Room = {
+//   title: String;
+//   imageRef: String;
+//   imageData: String;
+//   building: number;
+//   floor: number;
+//   capacity: number;
+//   reservations: Array<any>;
+// };
 
 // exports the State's initial values.
-export const state: State = {
+export const state = {
   user: "Guest",
   rooms: [],
   file: null,
+  search: "",
 };
