5afbc86d3a5b41e8bf5a43eb9ebdc40b20d3705b
Matthew Stiller <mstiller@ggc.edu>
2020-07-09 13:34:46 -0400

    Prepare structure for PATCH calls. - Add placeholder edit section - Add placeholder alert message on successful PATCH call

--- react/navigreat/frontend/components/Room.jsx
+++ react/navigreat/frontend/components/Room.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useOvermind } from "../overmind/config";
-import { Container, Content, CardItem, Text } from "native-base";
-import { Card } from "react-native-elements";
+import { Container, Content, CardItem, Form, Text, Item } from "native-base";
+import { Card, Input } from "react-native-elements";
+import { Button } from "react-native";
+
 import { Image } from "react-native";
 
 export default function Room({ route }) {
@@ -24,16 +26,25 @@ export default function Room({ route }) {
     },
   };
 
+  const payload = {};
+
   function Edit(props) {}
+
+  const handlePATCH = async () => {
+    alert(
+      "Your room update has been sent to the server.\n\
+      This button will PATCH the existing room.\n\
+      Refreshing the page..."
+    );
+    // TODO something with the updated payload
+  };
 
   return (
     <Container>
       <Content>
         <Card title={room.title}>
           <CardItem cardBody>
-            <Text>
-              
-            </Text>
+            <Text></Text>
             <Text style={styles.summary}>
               This is where the summary of a room would appear.
               <br />
@@ -50,6 +61,52 @@ export default function Room({ route }) {
             <Text style={styles.supplement}>The image should be ^ here.</Text>
           </CardItem>
         </Card>
+        {/* TODO conditional rendering for edit mode */}
+        <Form>
+          <Item>
+            <Input
+              placeholder="Title"
+              value={room.title}
+              onChangeText={(text) => (payload.title = text)}
+            />
+          </Item>
+          <Item>
+            <Input
+              placeholder="Summary"
+              value={room.summary}
+              onChangeText={(text) => (payload.summary = text)}
+            />
+          </Item>
+          <Item>
+            <Input
+              placeholder="Building"
+              value={room.building}
+              onChangeText={(text) => (payload.building = text)}
+            />
+          </Item>
+          <Item>
+            <Input
+              placeholder="Floor"
+              value={room.floor}
+              onChangeText={(text) => (payload.floor = text)}
+            />
+          </Item>
+          <Item>
+            <Input
+              placeholder="Number"
+              value={room.number}
+              onChangeText={(text) => (payload.number = text)}
+            />
+          </Item>
+          <Item>
+            <Input
+              placeholder="Capacity"
+              value={room.capacity}
+              onChangeText={(text) => (payload.capacity = text)}
+            />
+          </Item>
+          <Button title="Confirm Update" onPress={handlePATCH} />
+        </Form>
       </Content>
     </Container>
   );
