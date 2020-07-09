8fde7ca77090393d1b2d964655d1ad44ec87e8f3
Matthew Stiller <mstiller@ggc.edu>
2020-07-09 16:46:14 -0400

    Added payload tracking to OvermindJS state.

--- react/navigreat/frontend/overmind/state.ts
+++ react/navigreat/frontend/overmind/state.ts
@@ -1,17 +1,7 @@
-// type Room = {
-//   title: String;
-//   imageRef: String;
-//   imageData: String;
-//   building: number;
-//   floor: number;
-//   capacity: number;
-//   reservations: Array<any>;
-// };
-
-// exports the State's initial values.
 export const state = {
   user: "Guest",
   rooms: [],
+  payload: [],
   file: null,
   search: "",
 };
