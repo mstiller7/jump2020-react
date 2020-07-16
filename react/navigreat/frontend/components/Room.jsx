bde1272c6a396fa37f7e55d226aec0f0e07c3ee3
Matthew Stiller <mstiller@ggc.edu>
2020-07-16 18:33:22 -0400

    Room screen updates; conditional rendering! Sorta. - useEffect deps - handlePATCH() updates and TODOs

--- react/navigreat/frontend/components/Room.jsx
+++ react/navigreat/frontend/components/Room.jsx
@@ -37,91 +37,99 @@ export default function Room({ route }) {
     capacity: room.capacity,
     image: room.image,
   };
-
-  // TODO the toggling of the child state between edit mode.
-  function Edit(props) {}
-
-  // useEffect(() => {}, [JSON.stringify(room)]);
+  
+  useEffect(() => {}, [state.edit]);
 
   const handlePATCH = async () => {
     actions.postRoom(payload);
+    actions.refreshRooms();
     // TODO why doesn't the image re-appear?
-    actions.assignImages();
+    // TODO on subsequent refreshes, the summary isn't saved
+    // actions.assignImages();
+    actions.toggleEdit();
     // TODO replace image base64 payload with the ID ref again
     // TODO confirmation alert upon success
   };
 
-  return (
-    <Container>
-      <Content>
-        <Card title={room.title}>
-          <CardItem cardBody>
-            <Text></Text>
-            <Text style={styles.summary}>
-              {room.summary}
-              <br />
-              This is where the summary of a room would appear.
-              <br />
-              If it had one...
-            </Text>
-          </CardItem>
-          <CardItem>
-            <Image
-              style={{ height: 500, width: null, flex: 1 }}
-              source={{ uri: `data:image/;base64,${room.base64}` }}
-            />
-          </CardItem>
-          <CardItem cardBody>
-            <Text style={styles.supplement}>The image should be ^ here.</Text>
-          </CardItem>
-        </Card>
-        {/* TODO conditional rendering for edit mode */}
-        <Form>
-          <Item>
-            <Input
-              placeholder="Title"
-              defaultValue={room.title}
-              onChangeText={(text) => (payload.title = text)}
-            />
-          </Item>
-          <Item>
-            <Input
-              placeholder="Summary"
-              defaultValue={room.summary}
-              onChangeText={(text) => (payload.summary = text)}
-            />
-          </Item>
-          <Item>
-            <Input
-              placeholder="Building"
-              defaultValue={room.building}
-              onChangeText={(text) => (payload.building = text)}
-            />
-          </Item>
-          <Item>
-            <Input
-              placeholder="Floor"
-              defaultValue={room.floor}
-              onChangeText={(text) => (payload.floor = text)}
-            />
-          </Item>
-          <Item>
-            <Input
-              placeholder="Number"
-              defaultValue={room.number}
-              onChangeText={(text) => (payload.number = text)}
-            />
-          </Item>
-          <Item>
-            <Input
-              placeholder="Capacity"
-              defaultValue={room.capacity}
-              onChangeText={(text) => (payload.capacity = text)}
-            />
-          </Item>
-          <Button title="Confirm Update" onPress={handlePATCH} />
-        </Form>
-      </Content>
-    </Container>
-  );
+  if (state.edit === false)
+    return (
+      <Container>
+        <Content>
+          <Button title="Edit..." onPress={actions.toggleEdit} />
+          <Card title={room.title}>
+            <CardItem cardBody>
+              <Text></Text>
+              <Text style={styles.summary}>
+                {room.summary}
+                <br />
+                This is where the summary of a room would appear.
+                <br />
+                If it had one...
+              </Text>
+            </CardItem>
+            <CardItem>
+              <Image
+                style={{ height: 500, width: null, flex: 1 }}
+                source={{ uri: `data:image/;base64,${room.base64}` }}
+              />
+            </CardItem>
+            <CardItem cardBody>
+              <Text style={styles.supplement}>The image should be ^ here.</Text>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  else
+    return (
+      <Container>
+        <Content>
+          <Form>
+            <Item>
+              <Input
+                placeholder="Title"
+                defaultValue={room.title}
+                onChangeText={(text) => (payload.title = text)}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="Summary"
+                defaultValue={room.summary}
+                onChangeText={(text) => (payload.summary = text)}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="Building"
+                defaultValue={room.building}
+                onChangeText={(text) => (payload.building = text)}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="Floor"
+                defaultValue={room.floor}
+                onChangeText={(text) => (payload.floor = text)}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="Number"
+                defaultValue={room.number}
+                onChangeText={(text) => (payload.number = text)}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="Capacity"
+                defaultValue={room.capacity}
+                onChangeText={(text) => (payload.capacity = text)}
+              />
+            </Item>
+            <Button title="Confirm Update" onPress={handlePATCH} />
+          </Form>
+        </Content>
+      </Container>
+    );
 }
