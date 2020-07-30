e6a9f7d4d2513e21969d5fea14f914d42c7f2480
Matthew Stiller <mstiller@ggc.edu>
2020-07-30 15:07:31 -0400

    Change phone to string, add constructor & toString

--- java/java-bank/src/net/mstiller/dollars/model/Customer.java
+++ java/java-bank/src/net/mstiller/dollars/model/Customer.java
@@ -2,7 +2,20 @@ package net.mstiller.dollars.model;
 
 public class Customer {
 	
-	private String name, address;
-	private int phone;
+	public String name, address, phone;
 	
+	public Customer(String name, String address, String phone) {
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer{" +
+		"name='" + name + '\'' +
+		", address='" + address + '\'' +
+		", phone=" + phone + '\'' +
+		'}';
+	}
 }
