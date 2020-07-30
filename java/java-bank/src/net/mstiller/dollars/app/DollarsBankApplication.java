5acd02f62bfc72fb17d68a7ea8d3c40f75cf72f2
Matthew Stiller <mstiller@ggc.edu>
2020-07-30 14:02:30 -0400

    Add gitignore and colors.

--- java/java-bank/src/net/mstiller/dollars/app/DollarsBankApplication.java
+++ java/java-bank/src/net/mstiller/dollars/app/DollarsBankApplication.java
@@ -1,4 +1,14 @@
 package net.mstiller.dollars.app;
 
+import static net.mstiller.dollars.util.Colors.*;
+
 public class DollarsBankApplication {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
+		System.out.println(ANSI_GREEN + "This text is green!" + ANSI_RESET);
+		
+	}
+	
 }
