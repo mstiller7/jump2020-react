029944ca7cdc366eb29e31b0fceace63c89952ce
Matthew Stiller <mstiller@ggc.edu>
2020-07-30 15:07:43 -0400

    Fill out account fields.

--- java/java-bank/src/net/mstiller/dollars/model/Account.java
+++ java/java-bank/src/net/mstiller/dollars/model/Account.java
@@ -2,8 +2,22 @@ package net.mstiller.dollars.model;
 
 public class Account {
 	
-	private int id;
-	private String password;
-	private int balance;
+	public int id;
+	public String password;
+	public int balance;
 	
+	public Account(int id, String password, int balance) {
+		this.id = id;
+		this.password = password;
+		this.balance = balance;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account{" +
+		"id=" + id +
+		", password='" + password + '\'' +
+		", balance=" + balance +
+		'}';
+	}
 }
--- java/java-bank/src/net/mstiller/dollars/model/AccountSavings.java
+++ java/java-bank/src/net/mstiller/dollars/model/AccountSavings.java
@@ -1,7 +1,9 @@
 package net.mstiller.dollars.model;
 
 public class AccountSavings extends Account {
-
-
-
+	
+	public AccountSavings(int id, String password, int balance) {
+		super(id, password, balance);
+	}
+	
 }
