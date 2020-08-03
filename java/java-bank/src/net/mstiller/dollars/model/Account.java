c015b8a5a8aabb1c178e65b71a0de450d8d54332
Matthew Stiller <mstiller@ggc.edu>
2020-08-03 11:17:22 -0400

    Re-aligned Account layouts.

--- java/java-bank/src/net/mstiller/dollars/model/Account.java
+++ java/java-bank/src/net/mstiller/dollars/model/Account.java
@@ -1,19 +1,30 @@
 package net.mstiller.dollars.model;
 
+import java.util.ArrayList;
+
 public class Account {
 	
 	public String name, address, phone;
-	public int id;
+	public String id;
 	public String password;
 	public int balance;
+	public ArrayList<Transaction> transactions;
 	
-	public Account(String name, String address, String phone, int id, String password, int balance) {
+	public Account(String name, String address, String phone, String id, String password, int balance) {
 		this.name = name;
 		this.address = address;
 		this.phone = phone;
 		this.id = id;
 		this.password = password;
 		this.balance = balance;
+		this.transactions = new ArrayList<>();
+	}
+	
+	public void recordTransaction(Transaction t) {
+		transactions.add(t);
+		if (transactions.size() > 5) {
+			transactions.remove(0);
+		}
 	}
 	
 	@Override
--- java/java-bank/src/net/mstiller/dollars/model/AccountSavings.java
+++
@@ -1,8 +0,0 @@
-package net.mstiller.dollars.model;
-
-public class AccountSavings extends Account {
-	
-	public AccountSavings(String name, String address, String phone, int id, String password, int balance) {
-		super(name, address, phone, id, password, balance);
-	}
-}
