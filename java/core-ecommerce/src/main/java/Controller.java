76eff93547df90f933c4181fb360508db7065759
Matthew Stiller <mstiller@ggc.edu>
2020-09-21 14:09:01 -0400

    Basic Controller layout.

--- java/core-ecommerce/src/main/java/Controller.java
+++ java/core-ecommerce/src/main/java/Controller.java
@@ -4,4 +4,41 @@ public class Controller {
 	
 	}
 	
+	public void landing() {
+	
+	}
+	
+	public void register() {
+	
+	}
+	
+	public void login() {
+	
+	}
+	
+	public void menu() {
+	
+	}
+	
+	public void order() {
+	
+	}
+	
+	public void replace() {
+	
+	}
+	
+	/*
+	 * register
+	 * login
+	 * show items available
+	 * go to checkout
+	 *
+	 * menu options:
+	 * register
+	 * login
+	 * make order
+	 * make replacement
+	 */
+	
 }
